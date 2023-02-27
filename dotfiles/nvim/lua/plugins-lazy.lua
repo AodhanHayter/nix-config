898d5da27de5302a0be9084adcea848c0ac3e1a5
Aodhan Hayter <aodhan.hayter@gmail.com>
2023-02-27 14:48:58 -0700

    Get snippets working in completion

--- dotfiles/nvim/lua/plugins-lazy.lua
+++ dotfiles/nvim/lua/plugins-lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
     config = function()
       vim.cmd([[colorscheme nova]])
       vim.cmd([[highlight clear SignColumn]])
-    end,
+    end
   },
   { "numToStr/Comment.nvim", lazy = false, config = true },
   { "windwp/nvim-autopairs", lazy = false, config = true },
@@ -45,23 +45,60 @@ require("lazy").setup({
     dependencies = { "nvim-tree/nvim-web-devicons" },
     tag = "nightly"
   },
+  { "L3MON4D3/LuaSnip", lazy = false, version = "1.2.1",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function() require("luasnip.loaders.from_vscode").lazy_load() end },
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline" },
+      "hrsh7th/cmp-cmdline", "saadparwaiz1/cmp_luasnip" },
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
       cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        },
+        window = {
+          completion = cmp.config.window.bordered()
+        },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" })
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
+          { name = "luasnip" }
         }, {
           { name = "buffer" },
         })
@@ -72,8 +109,11 @@ require("lazy").setup({
   { "lewis6991/gitsigns.nvim", config = true, dependencies = { "nvim-lua/plenary.nvim" } },
   "tpope/vim-fugitive",
   "ellisonleao/glow.nvim",
-  { "iamcco/markdown-preview.nvim", build = "cd app && npm install", ft = "markdown",
-    config = function() vim.g.mkdp_filetypes = { "markdown" } end },
+  { "iamcco/markdown-preview.nvim", build = ":call mkdp#util#install()", ft = "markdown",
+    config = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end
+  },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
 })
 
@@ -150,13 +190,13 @@ require('lspconfig')['lua_ls'].setup {
   }
 }
 
-require('lspconfig')['bashls'].setup{
+require('lspconfig')['bashls'].setup {
   on_attach = on_attach,
   flags = lsp_flags,
   capabilities = capabilities,
 }
 
-require('lspconfig')['rnix'].setup{
+require('lspconfig')['rnix'].setup {
   on_attach = on_attach,
   flags = lsp_flags,
   capabilities = capabilities,
